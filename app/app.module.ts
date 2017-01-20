17481f8671fa38333e5b9d17c1f9faffdbde2dcc
Pat Blair <patblair@dkmbp.local>
2017-01-20 12:31:33 -0500

    page toh-pt5.html of the tutorial at the Router Outlet section

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,36 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+
+import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent,
+      }
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+  ],
+  providers: [
+    HeroService,
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
 })
+
 export class AppModule { }
